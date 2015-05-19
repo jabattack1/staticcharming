14490008dbc6836e0c2eba2cc589070274bc80e9
jabattack1 <peter.hahn@live.com>
2015-05-19 12:37:58 -0400

    static site

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('connected');
+
+
+// $(document).ready(function() {
+//   $("#cf_onclick").click(function() {
+//   $("#cf img.top").toggleClass("transparent");
+// });
+// })
+
+// setInterval("changeImage()", 30000);
+
+
+
+ function startTimer() {
+              setInterval($("#cf img.top").toggleClass("transparent");, 3000);
+          }
